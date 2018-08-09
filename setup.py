2050219ef7311cfda0115e22b7c7f09465f975b3
Steve Chen <32351673+igp-gravity@users.noreply.github.com>
2018-08-09 17:41:24 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name='detrend-example',
       author='Zhangbei',
       author_email='rular099@gmail.com',
       packages=find_packages(where='.'),
+      py_modules=['detrend_example','main']
       install_requires=['numpy','matplotlib','pandas','scipy'],
      )
